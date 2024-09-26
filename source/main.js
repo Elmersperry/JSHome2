de29d9b96fc0ca45a47bd185b1c91171747e3bd6
Elmersperry <rodionstudent@mail.ru>
2024-09-26 18:57:10 +0300

    jjj

--- source/main.js
+++ source/main.js
@@ -1,36 +1,73 @@
 let car = {
-    manufacturer: "Tesla",
-    model: "Tesla Model 3",
-    year_of_release: "2017",
-    speed: "140 km/h",
+  manufacturer: "Tesla",
+  model: "Tesla Model 3",
+  year_of_release: "2017",
+  speed: "140 km/h",
 };
 
 alert(JSON.stringify(car));
 
 function STime() {
-    const v = 140;
-    let s = Number(document.getElementById("s1").value);
-    let t = s / v;
-    let stop = Math.trunc(t / 4);
-    alert("Необходимое время: " + Math.trunc(t + stop) + " часов");
+  const v = 140;
+  let s = Number(document.getElementById("s1").value);
+  let t = s / v;
+  let stop = Math.trunc(t / 4);
+  alert("Необходимое время: " + Math.trunc(t + stop) + " часов");
 }
 
 window.onload = () => {
-    let buttonCalc = document.getElementById("b1");
-    buttonCalc.addEventListener("click", STime);
-}
-
+  let buttonCalc = document.getElementById("b1").value;
+  buttonCalc.addEventListener("click", STime);
+};
 
 let newobject = {
-    chis: "",
-    znam: "",
-}
+  chis: Number(12),
+  znam: Number(3),
+};
 
 function Plus() {
-    alert(chis + znam);
+  alert(chis + znam);
 }
 
 window.onload = () => {
-    let buttonPlus = document.getElementById("").value;
-    buttonPlus.addEventListener("click", Plus);
+  let buttonPlus = document.getElementById("p1").value;
+  buttonPlus.addEventListener("click", Plus);
+};
+
+/*
+function Plus() {
+  alert(chis + znam);
 }
+
+window.onload = () => {
+  let buttonPlus = document.getElementById("buttonPlus").value;
+  buttonPlus.addEventListener("click", Plus);
+};
+
+function Plus() {
+  alert(chis + znam);
+}
+
+window.onload = () => {
+  let buttonPlus = document.getElementById("buttonPlus").value;
+  buttonPlus.addEventListener("click", Plus);
+};
+
+function Plus() {
+  alert(chis + znam);
+}
+
+window.onload = () => {
+  let buttonPlus = document.getElementById("buttonPlus").value;
+  buttonPlus.addEventListener("click", Plus);
+};
+
+function Plus() {
+  alert(chis + znam);
+}
+
+window.onload = () => {
+  let buttonPlus = document.getElementById("buttonPlus").value;
+  buttonPlus.addEventListener("click", Plus);
+};
+*/
